3b6837b30a8fe28cd50d5da976f74c6b16885b88
lewis wege <wegelewis7@gmail.com>
2024-02-24 16:32:14 +0300

    improved on code logic

--- script.js
+++ script.js
@@ -2,21 +2,40 @@
 let playGame = confirm("Shall we play Rock, Papper, Scissors?");
 
 if (playGame) {
-    let playerChoice = prompt("Please enter rock, papper or scissors.");
-    if(playerChoice) {
+    let playerChoice = prompt("Please enter rock, paper or scissors.");
+    if(playerChoice){
         let playerOne = playerChoice.trim().toLowerCase();
         if(
             playerOne === "rock" ||
-            playerOne === "pappper"||
+            playerOne === "paper"||
             playerOne === "scissors"
-        ) {
+        ){
             let computerChoice = Math.floor(Math.random() * 3 + 1);
             let computer =
             computerChoice === 1 
-            ? "rock"
-            :computerChoice === 2
-            ? "papper"
-            :scissors
+                ? "rock"
+                :computerChoice === 2
+                ? "paper"
+                : "scissors";
+            
+            let result = 
+                playerOne === computer
+                    ? "Tie game!"
+                    : playerOne === "rock" && computer === "paper"
+                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+                    : playerOne === "paper" && computer === "scissors"
+                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+                    : playerOne === "scissors" && computer === "rock"
+                    ? `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+                    : `playerOne: ${playerOne}\ncomputer: ${computer}\nyou win!`;
+            alert(result);
+            let playAgain = confirm("Play Again?");
+            playAgain ? location.reload() : alert("Ok, thanks for playing.");
+
         }
+    } else {
+        alert("I guess you have changed your mind, maybe next time.")
     }
+} else {
+    alert("Ok, maybe next time.")
 }
